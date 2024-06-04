e0f628cb42677c472666f57bef21e63f1f0d22ac
raquel-1 <rocamartinezraquel@gmail.com>
2024-06-04 20:07:57 +0200

    chore: new credential

--- src/constants/credentials.js
+++ src/constants/credentials.js
@@ -1,5 +1,5 @@
-// export const APP_ID = 'ffd47089'
-// export const APP_KEY = '6205f8e5be3a9f317166ea7360553f74'
+export const APP_ID = 'ffd47089'
+export const APP_KEY = '6205f8e5be3a9f317166ea7360553f74'
 
-export const APP_ID = '71befeda'
-export const APP_KEY = '22c7f9d1b418c3e4ae5e974399ed2f33'
+// export const APP_ID = '71befeda'
+// export const APP_KEY = '22c7f9d1b418c3e4ae5e974399ed2f33'
