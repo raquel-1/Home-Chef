dc017d0ea75dd116c1c396a25aeea8b877a2284b
raquel-1 <rocamartinezraquel@gmail.com>
2024-06-04 20:13:07 +0200

    docs: todo new

--- src/constants/credentials.js
+++ src/constants/credentials.js
@@ -1,5 +1,6 @@
 export const APP_ID = 'ffd47089'
 export const APP_KEY = '6205f8e5be3a9f317166ea7360553f74'
 
+// new
 // export const APP_ID = '71befeda'
 // export const APP_KEY = '22c7f9d1b418c3e4ae5e974399ed2f33'
