740cd104ecffc3731528e655bab6f023e4f59a6e
raquel-1 <rocamartinezraquel@gmail.com>
2025-10-03 23:10:15 +0200

    fix: error vercel 2

--- api/recipes.js
+++ api/recipes.js
@@ -1,15 +1,16 @@
 // api/recipes.js
-import fetch from 'node-fetch'
-
 export default async function handler(req, res) {
   const { mealType, count } = req.query
-  const APP_ID = process.env.EDAMAM_APP_ID
-  const APP_KEY = process.env.EDAMAM_APP_KEY
-
-  const url = `https://api.edamam.com/search?q=&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${mealType}&to=${count}`
 
   try {
-    const response = await fetch(url)
+    const response = await fetch(
+      `https://api.edamam.com/search?q=&app_id=${process.env.VITE_EDAMAM_APP_ID}&app_key=${process.env.VITE_EDAMAM_APP_KEY}&mealType=${mealType}&to=${count}`,
+    )
+
+    if (!response.ok) {
+      throw new Error(`Edamam API error: ${response.statusText}`)
+    }
+
     const data = await response.json()
     res.status(200).json(data.hits.map((hit) => hit.recipe))
   } catch (err) {
