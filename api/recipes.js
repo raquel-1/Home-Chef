531bb858605924207d96cd71a66a6031045994a2
raquel-1 <rocamartinezraquel@gmail.com>
2025-10-04 00:03:29 +0200

    fix: error vercel 7

--- api/recipes.js
+++ api/recipes.js
@@ -1,10 +1,11 @@
 // /api/recipes.js
 export default async function handler(req, res) {
-  const appID = process.env.EDAMAM_APP_ID
-  const appKey = process.env.EDAMAM_APP_KEY
+  const appID = process.env.VITE_EDAMAM_APP_ID
+  const appKey = process.env.VITE_EDAMAM_APP_KEY
   const mealType = req.query.mealType || 'breakfast'
+  const count = req.query.count || 10
 
-  const url = `https://api.edamam.com/search?q=recipe&app_id=${appID}&app_key=${appKey}&mealType=${mealType}&to=10`
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=recipe&app_id=${appID}&app_key=${appKey}&mealType=${mealType}&to=${count}`
 
   try {
     const response = await fetch(url)
